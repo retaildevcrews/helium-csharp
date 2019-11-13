5ba6365f37aeb64708384ea62f52cee4a32c207c
bartr <bartr@outlook.com>
2019-11-13 12:26:14 -0600

    added -d arg (duration in seconds)

--- src/integration-test/config.cs
+++ src/integration-test/config.cs
@@ -8,6 +8,7 @@ namespace HeliumIntegrationTest
         public bool Random = false;
         public int SleepMs = 0;
         public int Threads = 0;
+        public int Duration = 0;
         public bool RunLoop = false;
         public bool RunWeb = false;
         public string Host = "http://localhost:4120";
