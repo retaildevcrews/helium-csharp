31e9d5632fc98707ee18f5837367399e9bb3c7ce
bartr <bartr@outlook.com>
2019-12-08 11:45:35 -0600

    updated healthz

--- src/app/HealthChecks/CosmosHealthCheck.cs
+++ src/app/HealthChecks/CosmosHealthCheck.cs
@@ -20,7 +20,7 @@ using System.Globalization;
 
 namespace Helium
 {
-    public class CosmosHealthCheck : IHealthCheck
+    public partial class CosmosHealthCheck : IHealthCheck
     {
         public static readonly string Description = "Cosmos DB Health Check";
 
@@ -77,7 +77,7 @@ namespace Helium
                 HealthStatus status = HealthStatus.Healthy;
 
                 // overall health is the worst status
-                foreach(var d in data.Values)
+                foreach (var d in data.Values)
                 {
                     if (status != HealthStatus.Unhealthy)
                     {
@@ -123,193 +123,6 @@ namespace Helium
 
                 return new HealthCheckResult(HealthStatus.Unhealthy, Description, ex, data);
             }
-        }
-
-        /// <summary>
-        /// Run the Get Genres Healthcheck
-        /// </summary>
-        /// <returns>HealthzResult</returns>
-        private async Task<HealthzResult> GetGenresAsync()
-        {
-            var result = new HealthzResult();
-
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
-            (await _dal.GetGenresAsync()).ToList<string>();
-            sw.Stop();
-            result.Uri = "/api/genres";
-            result.Status = HealthStatus.Healthy;
-            result.Duration = sw.Elapsed;
-
-            if (sw.ElapsedMilliseconds > 200)
-            {
-                result.Status = HealthStatus.Degraded;
-                result.Message = "Request exceeded expected duration";
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Run the Get Movie by Id Healthcheck
-        /// </summary>
-        /// <returns>HealthzResult</returns>
-        private async Task<HealthzResult> GetMovieByIdAsync(string movieId)
-        {
-            var result = new HealthzResult();
-
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
-            await _dal.GetMovieAsync(movieId);
-            sw.Stop();
-            result.Uri = string.Format($"/api/movies/{movieId}");
-            result.Status = HealthStatus.Healthy;
-            result.Duration = sw.Elapsed;
-
-            if (sw.ElapsedMilliseconds > 100)
-            {
-                result.Status = HealthStatus.Degraded;
-                result.Message = "Request exceeded expected duration";
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Run the Search Movies Healthcheck
-        /// </summary>
-        /// <returns>HealthzResult</returns>
-        private async Task<HealthzResult> SearchMoviesAsync(string query)
-        {
-            var result = new HealthzResult();
-
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
-            (await _dal.GetMoviesByQueryAsync(query)).ToList<Movie>();
-            sw.Stop();
-            result.Uri = string.Format($"/api/movies?q={query}");
-            result.Status = HealthStatus.Healthy;
-            result.Duration = sw.Elapsed;
-
-            if (sw.ElapsedMilliseconds > 100)
-            {
-                result.Status = HealthStatus.Degraded;
-                result.Message = "Request exceeded expected duration";
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Run the Get Top Rated Movies Healthcheck
-        /// </summary>
-        /// <returns>HealthzResult</returns>
-        private async Task<HealthzResult> GetTopRatedMoviesAsync()
-        {
-            var result = new HealthzResult();
-
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
-            (await _dal.GetMoviesByQueryAsync(string.Empty, toprated: true)).ToList<Movie>();
-            sw.Stop();
-            result.Uri = string.Format($"/api/movies?toprated=true");
-            result.Status = HealthStatus.Healthy;
-            result.Duration = sw.Elapsed;
-
-            if (sw.ElapsedMilliseconds > 100)
-            {
-                result.Status = HealthStatus.Degraded;
-                result.Message = "Request exceeded expected duration";
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Run the Get Actor By Id Healthcheck
-        /// </summary>
-        /// <returns>HealthzResult</returns>
-        private async Task<HealthzResult> GetActorByIdAsync(string actorId)
-        {
-            var result = new HealthzResult();
-
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
-            await _dal.GetActorAsync(actorId);
-            sw.Stop();
-            result.Uri = string.Format($"/api/actors/{actorId}");
-            result.Status = HealthStatus.Healthy;
-            result.Duration = sw.Elapsed;
-
-            if (sw.ElapsedMilliseconds > 100)
-            {
-                result.Status = HealthStatus.Degraded;
-                result.Message = "Request exceeded expected duration";
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Run the Search Actors Healthcheck
-        /// </summary>
-        /// <returns>HealthzResult</returns>
-        private async Task<HealthzResult> SearchActorsAsync(string query)
-        {
-            var result = new HealthzResult();
-
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
-            (await _dal.GetActorsByQueryAsync(query)).ToList<Actor>();
-            sw.Stop();
-            result.Uri = string.Format($"/api/actors?q={query}");
-            result.Status = HealthStatus.Healthy;
-            result.Duration = sw.Elapsed;
-
-            if (sw.ElapsedMilliseconds > 100)
-            {
-                result.Status = HealthStatus.Degraded;
-                result.Message = "Request exceeded expected duration";
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Write the health check results as json
-        /// </summary>
-        /// <param name="httpContext">HttpContext</param>
-        /// <param name="healthReport">HealthReport</param>
-        /// <returns></returns>
-        public static Task CustomResponseWriter(HttpContext httpContext, HealthReport healthReport)
-        {
-            httpContext.Response.ContentType = "application/json";
-
-            // setup serialization options
-            if (jsonOptions == null)
-            {
-                // ignore nulls in json
-                jsonOptions = new JsonSerializerOptions
-                {
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                    IgnoreNullValues = true
-                };
-
-                // serialize enums as strings
-                jsonOptions.Converters.Add(new JsonStringEnumConverter());
-
-                // serialize TimeSpan as 00:00:00.1234567
-                jsonOptions.Converters.Add(new TimeSpanConverter());
-            }
-
-            // write the json
-            return httpContext.Response.WriteAsync(JsonSerializer.Serialize(healthReport, jsonOptions));
         }
     }
 }
