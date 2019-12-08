14085cffbd5c02b085481ab1c2f294fb12783fc0
bartr <bartr@outlook.com>
2019-12-08 15:52:37 -0600

    refactored details

--- src/app/HealthChecks/CosmosHealthCheckDetails.cs
+++ src/app/HealthChecks/CosmosHealthCheckDetails.cs
@@ -1,5 +1,6 @@
 using Helium.Model;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +9,34 @@ namespace Helium
 {
     public partial class CosmosHealthCheck : IHealthCheck
     {
-        // TODO - refactor - a lot of code can be shared
+        private readonly Stopwatch stopwatch = new Stopwatch();
+
+        /// <summary>
+        /// Build the response
+        /// </summary>
+        /// <param name="uri">string</param>
+        /// <param name="duration">TimeSpan</param>
+        /// <param name="targetDurationMs">double (ms)</param>
+        /// <returns>HealthzCheck</returns>
+        private HealthzCheck BuildHealthzCheck(string uri, TimeSpan duration, double targetDurationMs)
+        {
+            // create the result
+            var result = new HealthzCheck
+            {
+                Uri = uri,
+                Status = HealthStatus.Healthy,
+                Duration = duration
+            };
+
+            // check duration
+            if (duration.TotalMilliseconds > targetDurationMs)
+            {
+                result.Status = HealthStatus.Degraded;
+                result.Message = "Request exceeded expected duration";
+            }
+
+            return result;
+        }
 
         /// <summary>
         /// Get Genres Healthcheck
@@ -16,24 +44,11 @@ namespace Helium
         /// <returns>HealthzResult</returns>
         private async Task<HealthzCheck> GetGenresAsync()
         {
-            var result = new HealthzCheck();
-
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
+            stopwatch.Restart();
             (await _dal.GetGenresAsync()).ToList<string>();
-            sw.Stop();
-            result.Uri = "/api/genres";
-            result.Status = HealthStatus.Healthy;
-            result.Duration = sw.Elapsed;
+            stopwatch.Stop();
 
-            if (sw.ElapsedMilliseconds > 200)
-            {
-                result.Status = HealthStatus.Degraded;
-                result.Message = "Request exceeded expected duration";
-            }
-
-            return result;
+            return BuildHealthzCheck("/api/genres", stopwatch.Elapsed, 200);
         }
 
         /// <summary>
@@ -42,24 +57,13 @@ namespace Helium
         /// <returns>HealthzResult</returns>
         private async Task<HealthzCheck> GetMovieByIdAsync(string movieId)
         {
-            var result = new HealthzCheck();
-
             Stopwatch sw = new Stopwatch();
 
             sw.Start();
             await _dal.GetMovieAsync(movieId);
             sw.Stop();
-            result.Uri = string.Format($"/api/movies/{movieId}");
-            result.Status = HealthStatus.Healthy;
-            result.Duration = sw.Elapsed;
 
-            if (sw.ElapsedMilliseconds > 100)
-            {
-                result.Status = HealthStatus.Degraded;
-                result.Message = "Request exceeded expected duration";
-            }
-
-            return result;
+            return BuildHealthzCheck($"/api/movies/{movieId}", sw.Elapsed, 200);
         }
 
         /// <summary>
@@ -68,24 +72,11 @@ namespace Helium
         /// <returns>HealthzResult</returns>
         private async Task<HealthzCheck> SearchMoviesAsync(string query)
         {
-            var result = new HealthzCheck();
-
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
+            stopwatch.Restart();
             (await _dal.GetMoviesByQueryAsync(query)).ToList<Movie>();
-            sw.Stop();
-            result.Uri = string.Format($"/api/movies?q={query}");
-            result.Status = HealthStatus.Healthy;
-            result.Duration = sw.Elapsed;
+            stopwatch.Stop();
 
-            if (sw.ElapsedMilliseconds > 100)
-            {
-                result.Status = HealthStatus.Degraded;
-                result.Message = "Request exceeded expected duration";
-            }
-
-            return result;
+            return BuildHealthzCheck($"/api/movies?q={query}", stopwatch.Elapsed, 200);
         }
 
         /// <summary>
@@ -94,24 +85,11 @@ namespace Helium
         /// <returns>HealthzResult</returns>
         private async Task<HealthzCheck> GetTopRatedMoviesAsync()
         {
-            var result = new HealthzCheck();
-
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
+            stopwatch.Restart();
             (await _dal.GetMoviesByQueryAsync(string.Empty, toprated: true)).ToList<Movie>();
-            sw.Stop();
-            result.Uri = string.Format($"/api/movies?toprated=true");
-            result.Status = HealthStatus.Healthy;
-            result.Duration = sw.Elapsed;
+            stopwatch.Stop();
 
-            if (sw.ElapsedMilliseconds > 100)
-            {
-                result.Status = HealthStatus.Degraded;
-                result.Message = "Request exceeded expected duration";
-            }
-
-            return result;
+            return BuildHealthzCheck("/api/movies?toprated=true", stopwatch.Elapsed, 200);
         }
 
         /// <summary>
@@ -120,24 +98,11 @@ namespace Helium
         /// <returns>HealthzResult</returns>
         private async Task<HealthzCheck> GetActorByIdAsync(string actorId)
         {
-            var result = new HealthzCheck();
-
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
+            stopwatch.Restart();
             await _dal.GetActorAsync(actorId);
-            sw.Stop();
-            result.Uri = string.Format($"/api/actors/{actorId}");
-            result.Status = HealthStatus.Healthy;
-            result.Duration = sw.Elapsed;
+            stopwatch.Stop();
 
-            if (sw.ElapsedMilliseconds > 100)
-            {
-                result.Status = HealthStatus.Degraded;
-                result.Message = "Request exceeded expected duration";
-            }
-
-            return result;
+            return BuildHealthzCheck($"/api/actors/{actorId}", stopwatch.Elapsed, 200);
         }
 
         /// <summary>
@@ -146,24 +111,11 @@ namespace Helium
         /// <returns>HealthzResult</returns>
         private async Task<HealthzCheck> SearchActorsAsync(string query)
         {
-            var result = new HealthzCheck();
-
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
+            stopwatch.Restart();
             (await _dal.GetActorsByQueryAsync(query)).ToList<Actor>();
-            sw.Stop();
-            result.Uri = string.Format($"/api/actors?q={query}");
-            result.Status = HealthStatus.Healthy;
-            result.Duration = sw.Elapsed;
+            stopwatch.Stop();
 
-            if (sw.ElapsedMilliseconds > 100)
-            {
-                result.Status = HealthStatus.Degraded;
-                result.Message = "Request exceeded expected duration";
-            }
-
-            return result;
+            return BuildHealthzCheck($"/api/actors?q={query}", stopwatch.Elapsed, 200);
         }
     }
 }
