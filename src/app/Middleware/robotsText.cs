b7680ec8176eec30e2b3c3d5d86eadbaf6fbedee
bartr <bartr@outlook.com>
2019-12-25 16:11:40 -0600

    updated comments

--- src/app/Middleware/robotsText.cs
+++ src/app/Middleware/robotsText.cs
@@ -3,13 +3,16 @@ using System;
 
 namespace Helium
 {
+    /// <summary>
+    /// Register robots middleware
+    /// </summary>
     public static class RobotsMiddlewareExtensions
     {
         // response that prevents all indexing
         static readonly byte[] _responseBytes = System.Text.Encoding.UTF8.GetBytes("# Prevent indexing\r\nUser-agent: *\r\nDisallow: /\r\n");
 
         /// <summary>
-        /// Middleware extension method to handle /robots*.txt request
+        /// aspnet middleware extension method to handle /robots*.txt request
         /// App Service sends a warmup request of /robots8327.txt (where 8327 is a random number)
         /// this causes a 404 error which appears in Azure Monitor as a false issue
         /// this also handles a real /robots.txt request to prevent indexing
@@ -21,7 +24,7 @@ namespace Helium
             // create the middleware
             builder.Use(async (context, next) =>
             {
-                // convert to lower and remove the leading /
+                // remove the leading /
                 string path = context.Request.Path.Value.Substring(1);
 
                 // matches /robots*.txt (/robots.txt /robots123.txt etc)
