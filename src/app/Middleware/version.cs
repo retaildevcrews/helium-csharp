27cb162b620b46fd93196e2756d6beb159534553
bartr <bartr@outlook.com>
2019-12-25 16:13:47 -0600

    updated comments

--- src/app/Middleware/version.cs
+++ src/app/Middleware/version.cs
@@ -2,30 +2,34 @@ using Microsoft.AspNetCore.Builder;
 
 namespace Helium
 {
+    /// <summary>
+    /// Registers aspnet middleware handler that handles /version
+    /// </summary>
     public static class VersionMiddlewareExtensions
     {
-        // response that returns the current version
+        // cached response
         static byte[] _responseBytes = null;
 
         /// <summary>
         /// Middleware extension method to handle /version request
         /// </summary>
         /// <param name="builder">this IApplicationBuilder</param>
-        /// <returns></returns>
+        /// <returns>IApplicationBuilder</returns>
         public static IApplicationBuilder UseVersion(this IApplicationBuilder builder)
         {
-            // create the middleware
+            // implement the middleware
             builder.Use(async (context, next) =>
             {
                 // matches /version
                 if (context.Request.Path.Value.Equals("/version", System.StringComparison.OrdinalIgnoreCase))
                 {
+                    // cache the version info for performance
                     if (_responseBytes == null)
                     {
                         _responseBytes = System.Text.Encoding.UTF8.GetBytes(Version.AssemblyVersion);
                     }
 
-                    // return the content
+                    // return the version info
                     context.Response.ContentType = "text/plain";
                     await context.Response.Body.WriteAsync(_responseBytes, 0, _responseBytes.Length).ConfigureAwait(false);
                 }
