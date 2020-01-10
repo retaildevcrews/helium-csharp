b389b13296070cb6c09eeca5a68fd7a9cd97a429
Bart Robertson <bartr@microsoft.com>
2020-01-10 15:31:39 -0600

    Updated nuget packages (#33)
    * updated nuget packages

--- src/app/Model/Actor.cs
+++ src/app/Model/Actor.cs
@@ -1,34 +1,28 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Helium.Model
 {
-    public class ActorBase
+    public class Actor
     {
-        [JsonProperty(Order = 2)]
+        public string Id { get; set; }
         public string ActorId { get; set; }
-        [JsonProperty(Order = 10)]
         public string Name { get; set; }
-        [JsonProperty(Order = 11)]
         public int BirthYear { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore, Order = 12)]
-        public int DeathYear { get; set; }
+        public int? DeathYear { get; set; }
+        public string PartitionKey { get; set; }
+        public List<string> Profession { get; set; }
+        public string Type { get; set; }
+        public string TextSearch { get; set; }
+        public List<ActorMovie> Movies { get; set; }
     }
 
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "breaks json serialization")]
-    public class Actor : ActorBase
+    public class ActorMovie
     {
-        [JsonProperty(Order = 1)]
-        public string Id { get; set; }
-        [JsonProperty(Order = 3)]
-        public string PartitionKey { get; set; }
-        [JsonProperty(Order = 96)]
-        public List<string> Profession { get; set; }
-        [JsonProperty(Order = 97)]
+        public string MovieId { get; set; }
         public string Type { get; set; }
-        [JsonProperty(Order = 98)]
-        public string TextSearch { get; set; }
-        [JsonProperty(Order = 99, NullValueHandling = NullValueHandling.Ignore)]
-        public List<MovieBase> Movies { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public int Runtime { get; set; }
+        public List<string> Genres { get; set; }
     }
 }
