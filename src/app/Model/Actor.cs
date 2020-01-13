67d56942540a9fd47dcc68ba6689952debae445f
Bart Robertson <bartr@microsoft.com>
2020-01-13 13:49:06 -0600

    updated json serialization (#35)
    * updated model
    * removed newtonsoft.json

--- src/app/Model/Actor.cs
+++ src/app/Model/Actor.cs
@@ -6,20 +6,19 @@ namespace Helium.Model
     {
         public string Id { get; set; }
         public string ActorId { get; set; }
-        public string Name { get; set; }
-        public int BirthYear { get; set; }
-        public int? DeathYear { get; set; }
         public string PartitionKey { get; set; }
-        public List<string> Profession { get; set; }
         public string Type { get; set; }
+        public string Name { get; set; }
+        public int? BirthYear { get; set; }
+        public int? DeathYear { get; set; }
         public string TextSearch { get; set; }
+        public List<string> Profession { get; set; }
         public List<ActorMovie> Movies { get; set; }
     }
 
     public class ActorMovie
     {
         public string MovieId { get; set; }
-        public string Type { get; set; }
         public string Title { get; set; }
         public int Year { get; set; }
         public int Runtime { get; set; }
--- src/app/Model/Movie.cs
+++ src/app/Model/Movie.cs
@@ -11,11 +11,11 @@ namespace Helium.Model
         public string Title { get; set; }
         public int Year { get; set; }
         public int Runtime { get; set; }
-        public List<string> Genres { get; set; }
         public double Rating { get; set; }
         public long Votes { get; set; }
         public long TotalScore { get; set; }
         public string TextSearch { get; set; }
+        public List<string> Genres { get; set; }
         public List<Role> Roles { get; set; }
     }
 
@@ -24,15 +24,9 @@ namespace Helium.Model
         public int Order { get; set; }
         public string ActorId { get; set; }
         public string Name { get; set; }
-        public int BirthYear { get; set; }
+        public int? BirthYear { get; set; }
         public int? DeathYear { get; set; }
         public string Category { get; set; }
         public List<string> Characters { get; set; }
-    }
-
-    public class FeaturedMovie
-    {
-        public string MovieId { get; set; }
-        public int Weight { get; set; } = 1;
     }
 }
