55391fcf87fb9fcb0e909348e2ebb2814cc62df6
bartr <bartr@microsoft.com>
2020-03-02 04:08:34 -0600

    added query string validation

--- src/app/Controllers/ActorsController.cs
+++ src/app/Controllers/ActorsController.cs
@@ -44,6 +44,18 @@ namespace Helium.Controllers
         {
             string method = GetMethod(q, pageNumber, pageSize);
 
+            // validate query string parameters
+            if (!QueryStringValidator.Common(HttpContext.Request.Query, q, pageNumber, pageSize, out string message))
+            {
+                _logger.LogWarning($"InvalidParameter|{method}|{message}");
+
+                return new ContentResult
+                {
+                    Content = message,
+                    StatusCode = (int)System.Net.HttpStatusCode.BadRequest
+                };
+            }
+
             _logger.LogInformation(method);
 
             try
@@ -72,8 +84,9 @@ namespace Helium.Controllers
                 // log and return Cosmos status code
                 _logger.LogError($"CosmosException:{method}:{ce.StatusCode}:{ce.ActivityId}:{ce.Message}\n{ce}");
 
-                return new ObjectResult(Constants.ActorsControllerException)
+                return new ContentResult
                 {
+                    Content = Constants.ActorsControllerException,
                     StatusCode = (int)ce.StatusCode
                 };
             }
@@ -90,8 +103,9 @@ namespace Helium.Controllers
                 // log and return 500
                 _logger.LogError($"AggregateException|{method}|{root.GetType()}|{root.Message}|{root.Source}|{root.TargetSite}");
 
-                return new ObjectResult(Constants.ActorsControllerException)
+                return new ContentResult
                 {
+                    Content = Constants.ActorsControllerException,
                     StatusCode = (int)System.Net.HttpStatusCode.InternalServerError
                 };
             }
@@ -100,8 +114,9 @@ namespace Helium.Controllers
             {
                 _logger.LogError($"Exception:{method}\n{ex}");
 
-                return new ObjectResult(Constants.ActorsControllerException)
+                return new ContentResult
                 {
+                    Content = Constants.ActorsControllerException,
                     StatusCode = (int)System.Net.HttpStatusCode.InternalServerError
                 };
             }
@@ -119,6 +134,17 @@ namespace Helium.Controllers
         public async Task<IActionResult> GetActorByIdAsync(string actorId)
         {
             _logger.LogInformation($"GetActorByIdAsync {actorId}");
+
+            if (!QueryStringValidator.ActorId(actorId, out string message))
+            {
+                _logger.LogWarning($"GetActorByIdAsync|{actorId}|{message}");
+
+                return new ContentResult
+                {
+                    Content = message,
+                    StatusCode = (int)System.Net.HttpStatusCode.BadRequest
+                };
+            }
 
             try
             {
@@ -150,8 +176,9 @@ namespace Helium.Controllers
                     // log and return Cosmos status code
                     _logger.LogError($"CosmosException:GetActorByIdAsync:{ce.StatusCode}:{ce.ActivityId}:{ce.Message}\n{ce}");
 
-                    return new ObjectResult(Constants.ActorsControllerException)
+                    return new ContentResult
                     {
+                        Content = Constants.ActorsControllerException,
                         StatusCode = (int)ce.StatusCode
                     };
                 }
@@ -169,8 +196,9 @@ namespace Helium.Controllers
                 // log and return 500
                 _logger.LogError($"AggregateException|GetActorByIdAsync|{root.GetType()}|{root.Message}|{root.Source}|{root.TargetSite}");
 
-                return new ObjectResult(Constants.ActorsControllerException)
+                return new ContentResult
                 {
+                    Content = Constants.ActorsControllerException,
                     StatusCode = (int)System.Net.HttpStatusCode.InternalServerError
                 };
             }
@@ -180,8 +208,9 @@ namespace Helium.Controllers
             {
                 _logger.LogError($"Exception:GetActorByIdAsync:{e.Message}\n{e}");
 
-                return new ObjectResult(Constants.ActorsControllerException)
+                return new ContentResult
                 {
+                    Content = Constants.ActorsControllerException,
                     StatusCode = (int)System.Net.HttpStatusCode.InternalServerError
                 };
             }
---
+++ src/app/Controllers/QueryStringValidator.cs
@@ -0,0 +1,164 @@
+﻿using Helium.DataAccessLayer;
+using Helium.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Helium.Controllers
+{
+    /// <summary>
+    /// Validate the query string parameters
+    /// </summary>
+    public static class QueryStringValidator
+    {
+        /// <summary>
+        /// Validate parameters common between Actors and Movies
+        /// </summary>
+        /// <param name="query">IQueryCollection</param>
+        /// <param name="q">search</param>
+        /// <param name="pageNumber">Page Number</param>
+        /// <param name="pageSize">Page Size</param>
+        /// <param name="message">error message</param>
+        /// <returns></returns>
+        public static bool Common(IQueryCollection query, string q, int pageNumber, int pageSize, out string message)
+        {
+            message = string.Empty;
+
+            // no query string
+            if (query == null)
+            {
+                return true;
+            }
+
+            // validate q (search)
+            if (query.ContainsKey("q"))
+            {
+                if (q == null || q.Length < 3 || q.Length > 50)
+                {
+                    message = "Invalid q parameter";
+                    return false;
+                }
+            }
+
+            // validate page number
+            if (query.ContainsKey("pageNumber"))
+            {
+                if (pageNumber < 1 || pageNumber > 10000)
+                {
+                    message = "Invalid PageNumber parameter";
+                    return false;
+                }
+            }
+
+            // validate page size
+            if (query.ContainsKey("pageSize"))
+            {
+                if (pageSize < 1 || pageSize > 1000)
+                {
+                    message = "Invalid PageSize parameter";
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool Movies(IQueryCollection query, string q, string genre, int year, double rating, string actorId, int pageNumber, int pageSize, out string message)
+        {
+            message = string.Empty;
+
+            // no query string
+            if (query == null)
+            {
+                return true;
+            }
+
+            // validate q, page number and page size
+            if (!Common(query, q, pageNumber, pageSize, out message))
+            {
+                return false;
+            }
+
+            // validate genre
+            if (query.ContainsKey("genre"))
+            {
+                if (genre == null || genre.Length < 3 || genre.Length > 50)
+                {
+                    message = "Invalid Genre parameter";
+                    return false;
+                }
+            }
+
+            // validate year
+            if (query.ContainsKey("year"))
+            {
+                if (year < 1874 || year > DateTime.UtcNow.Year + 5)
+                {
+                    message = "Invalid Year parameter";
+                    return false;
+                }
+            }
+
+            // validate rating
+            if (query.ContainsKey("rating"))
+            {
+                if (rating < 0 || rating > 10)
+                {
+                    message = "Invalid Rating parameter";
+                    return false;
+                }
+            }
+
+            // validate actorId
+            if (query.ContainsKey("actorId"))
+            {
+                if (!ActorId(actorId, out message))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool ActorId(string actorId, out string message)
+        {
+            message = string.Empty;
+
+            // validate actorId
+            if (actorId == null ||
+                actorId.Length < 7 ||
+                actorId.Length > 11 ||
+                actorId.Substring(0, 2) != "nm" ||
+                !int.TryParse(actorId.Substring(2), out int _))
+            {
+                message = "Invalid Actor ID parameter";
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool MovieId(string movieId, out string message)
+        {
+            message = string.Empty;
+
+            // validate movieId
+            if (movieId == null ||
+                movieId.Length < 7 ||
+                movieId.Length > 11 ||
+                movieId.Substring(0, 2) != "tt" ||
+                !int.TryParse(movieId.Substring(2), out int _))
+            {
+                message = "Invalid Movie ID parameter";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
