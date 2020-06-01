ede7951cbe71d6c42cc649edde4c3e725e5054fb
Eddie Villalba <edvilla@microsoft.com>
2020-06-01 17:52:57 -0500

    Update CI workflow to support webv global tool testing (#95)
    * added TestFiles for webv testing process, update to a single CI file with tests and added webv
    
    * updated devcontainer files
    
    Co-authored-by: bartr <bartr@microsoft.com>

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -6,11 +6,8 @@ echo "post-install.sh ..." >> ~/status
 # copy vscode files
 mkdir -p .vscode && cp docs/vscode-template/* .vscode
 
-# run dotnet restore
-dotnet restore src/tests.sln
-
 # source the bashrc-append from the repo
-# you can add project specific settings to .bashrc-append and 
+# you can add project specific settings to .bashrc-append and
 # they will be added for every user that clones the repo with Codespaces
 # including keys or secrets could be a SECURITY RISK
 echo "" >> ~/.bashrc
@@ -25,9 +22,27 @@ sudo apt-get update
 date >> ~/status
 echo "Installing basics ..." >> ~/status
 
-sudo apt-get install -y --no-install-recommends apt-utils dialog curl git
+sudo apt-get install -y --no-install-recommends apt-utils dialog
 
 DEBIAN_FRONTEND=dialog
+
+date >> ~/status
+echo "Building helium ..." >> ~/status
+
+# add tools to path
+export PATH="$PATH:~/.dotnet/tools"
+
+# set dotnet root
+export DOTNET_ROOT=~/.dotnet
+
+# set auth type
+export AUTH_TYPE=CLI
+
+# install WebV
+dotnet tool install -g webvalidate --version 1.0.7.3
+
+# run dotnet restore
+dotnet restore src/tests.sln
 
 date >> ~/status
 echo "Done" >> ~/status
