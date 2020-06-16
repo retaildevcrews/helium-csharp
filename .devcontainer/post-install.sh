05d39dba90dccd02c0658b9de63543e7e0a9ffa7
bartr <bartr@microsoft.com>
2020-06-16 03:20:55 -0500

    added httpie

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -6,13 +6,14 @@ mkdir -p .vscode && cp docs/vscode-template/* .vscode
 # source the bashrc-append from the repo
 # you can add project specific settings to .bashrc-append and
 # they will be added for every user that clones the repo with Codespaces
-# including keys or secrets could be a SECURITY RISK
+# including keys or secrets is a SECURITY RISK
 echo "" >> ~/.bashrc
 echo ". ${PWD}/.devcontainer/.bashrc-append" >> ~/.bashrc
 
 DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends apt-utils dialog dnsutils
+sudo apt-get install -y --no-install-recommends apt-utils dialog
+sudo apt-get install -y --no-install-recommends dnsutils httpie
 DEBIAN_FRONTEND=dialog
 
 # install WebV global tool
