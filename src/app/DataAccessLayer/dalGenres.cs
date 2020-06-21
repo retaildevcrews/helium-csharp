9789a00ef607038d094eea1cdd3c1eb18215eecc
bartr <bartr@microsoft.com>
2020-06-21 16:25:51 -0500

    updated to version 1.1.0 for release

--- src/app/DataAccessLayer/dalGenres.cs
+++ src/app/DataAccessLayer/dalGenres.cs
@@ -1,4 +1,3 @@
-using Microsoft.Azure.Cosmos;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
--- src/app/DataAccessLayer/dalMovies.cs
+++ src/app/DataAccessLayer/dalMovies.cs
@@ -2,7 +2,6 @@ using CSE.Helium.Model;
 using Microsoft.Azure.Cosmos;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace CSE.Helium.DataAccessLayer
