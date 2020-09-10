3c46551e05f6a2767e20edcb3e76e1cdd127c9bf
Jason Shave <jasonshave@hotmail.com>
2020-09-10 08:59:10 -0600

    fixed formatting due to nested problemdetails object

--- src/app/Validation/ValidationProblemDetails.cs
+++ src/app/Validation/ValidationProblemDetails.cs
@@ -1,13 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CSE.Helium.Model;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
-using CSE.Helium.Model;
 
 namespace CSE.Helium.Validation
 {
-    public class ValidationProblemDetails : ProblemDetails
+    public class ValidationProblemDetails
     {
         private readonly List<ValidationError> validationErrors = new List<ValidationError>();
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("detail")]
+        public string Detail { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("instance")]
+        public string Instance { get; set; }
 
         [JsonPropertyName("validationErrors")]
         public ICollection<ValidationError> ValidationErrors => validationErrors;
