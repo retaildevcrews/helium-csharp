443a001d8d7e721e2ef46369eb7d8bbe6a232882
bartr <bartr@microsoft.com>
2020-09-11 00:35:37 -0500

    iis update

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -168,7 +168,8 @@ namespace CSE.Helium
             // configure the web host builder
             IWebHostBuilder builder = WebHost.CreateDefaultBuilder()
                 .UseConfiguration(config)
-                .UseKestrel()
+                //.UseIISIntegration()
+                //.UseKestrel()
                 .UseUrls(string.Format(System.Globalization.CultureInfo.InvariantCulture, $"http://*:{Constants.Port}/"))
                 .UseStartup<Startup>()
                 .UseShutdownTimeout(TimeSpan.FromSeconds(Constants.GracefulShutdownTimeout))
