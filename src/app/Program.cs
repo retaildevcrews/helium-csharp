0bc215199d9b3d239f2948ce848740d8bc6d37b2
bartr <bartr@microsoft.com>
2020-09-14 15:27:29 -0500

    updated startup message

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -109,7 +109,14 @@ namespace CSE.Helium
                 logger.LogInformation("Web Server Started");
             }
 
-            Console.WriteLine($"Version: {Middleware.VersionExtensions.Version}");
+            Console.WriteLine(" _          _ _");
+            Console.WriteLine("| |        | (_)");
+            Console.WriteLine("| |__   ___| |_ _   _ _ __ ___");
+            Console.WriteLine("| '_ \\ / _ \\ | | | | | '_ ` _ \\");
+            Console.WriteLine("| | | |  __/ | | |_| | | | | | |");
+            Console.WriteLine("|_| |_|\\___|_|_|\\__,_|_| |_| |_|");
+
+            Console.WriteLine($"\nVersion: {Middleware.VersionExtensions.Version}");
         }
 
         /// <summary>
