e8da5b732d2c3cb03baeb33dffc51486da929ad3
bartr <bartr@microsoft.com>
2020-09-29 19:23:40 -0500

    codespaces

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -6,33 +6,31 @@ cp .devcontainer/vscode-template/* .vscode
 
 # run dotnet restore
 dotnet restore src/helium.sln
+dotnet restore src/tests.sln
 
 # docker bash-completion
-sudo curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker
+#sudo curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker
 
 DEBIAN_FRONTEND=noninteractive
 # update apt-get
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends apt-utils dialog
+#sudo apt-get update
+#sudo apt-get install -y --no-install-recommends apt-utils dialog
 
 # update / install utils
 sudo apt-get install -y --no-install-recommends dnsutils httpie bash-completion curl wget git unzip
 DEBIAN_FRONTEND=dialog
 
-# run dotnet restore
-dotnet restore src/tests.sln
-
 # source the bashrc-append from the repo
 # you can add project specific settings to .bashrc-append and
 # they will be added for every user that clones the repo with Codespaces
 # including keys or secrets is a SECURITY RISK
-echo "" >> ~/.bashrc
-echo ". ${PWD}/.devcontainer/.bashrc-append" >> ~/.bashrc
+#echo "" >> ~/.bashrc
+#echo ". ${PWD}/.devcontainer/.bashrc-append" >> ~/.bashrc
 
 # install WebV global tool
-export PATH="$PATH:~/.dotnet/tools"
-export DOTNET_ROOT=~/.dotnet
+#export PATH="$PATH:~/.dotnet/tools"
+#export DOTNET_ROOT=~/.dotnet
 dotnet tool install -g webvalidate
 
 # set auth type
-export AUTH_TYPE=CLI
+#export AUTH_TYPE=CLI
