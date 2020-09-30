223d33824b71292fd53bb0f344347db16a8718a2
bartr <bartr@microsoft.com>
2020-09-30 03:51:09 -0500

    docker

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -8,24 +8,14 @@ cp .devcontainer/vscode-template/* .vscode
 dotnet restore src/helium.sln
 dotnet restore src/tests.sln
 
-# docker bash-completion
-#sudo curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker
-
-DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends dnsutils httpie bash-completion curl wget git unzip
-DEBIAN_FRONTEND=dialog
-
-# source the bashrc-append from the repo
-# you can add project specific settings to .bashrc-append and
-# they will be added for every user that clones the repo with Codespaces
-# including keys or secrets is a SECURITY RISK
-echo "" >> ~/.bashrc
-echo ". ${PWD}/.devcontainer/.bashrc-append" >> ~/.bashrc
+# set auth type
+export PATH="$PATH:~/.dotnet/tools"
+export AUTH_TYPE=CLI
 
 # install WebV global tool
-export PATH="$PATH:~/.dotnet/tools"
 dotnet tool install -g webvalidate
 
-# set auth type
-export AUTH_TYPE=CLI
+# update .bashrc
+echo "" >> ~/.bashrc
+echo 'export PATH="$PATH:~/.dotnet/tools"' >> ~/.bashrc
+echo "export AUTH_TYPE=CLI" >> ~/.bashrc
