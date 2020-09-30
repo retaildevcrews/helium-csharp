671a89942b481557dbfa4e617e486a6a51d0b411
LakshayKaushik <lakshaykaushik25@gmail.com>
2020-10-01 03:48:08 +0530

    Dockerfile containing all tools to run helium setup inside a docker container (#149)
    * Tested with helium setup steps. Works fine
    
    * updated codespaces
    
    * codespaces build
    
    * updated codespaces
    
    * updated codespaces
    
    * codespaces
    
    * codespaces
    
    * codespaces
    
    * codespaces
    
    * codespaces
    
    * codespaces
    
    * Create Dockerfile
    
    * Delete Dockerfile
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * Delete Dockerfile
    
    * updates to get docker cli working inside the container. use custom image for codespaces. was able to get helium up and running with custom image
    
    * Cleanup comments
    
    * Newlines
    
    * issue #146 use prebuilt docker image instead of building on codespace startup
    
    Co-authored-by: bartr <bartr@microsoft.com>
    Co-authored-by: Akinlolu Akindele <aakindel@microsoft.com>
    Co-authored-by: sivamu <67930841+sivamu@users.noreply.github.com>

---
+++ .devcontainer/library-scripts/docker-debian.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# Install Docker CLI
+curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]')/gpg | (OUT=$(apt-key add - 2>&1) || echo $OUT)
+echo "deb [arch=amd64] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get -y install --no-install-recommends docker-ce-cli
+
+# Install Docker Compose
+LATEST_COMPOSE_VERSION=$(curl -sSL "https://api.github.com/repos/docker/compose/releases/latest" | grep -o -P '(?<="tag_name": ").+(?=")')
+curl -sSL "https://github.com/docker/compose/releases/download/${LATEST_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+echo "Done!"
