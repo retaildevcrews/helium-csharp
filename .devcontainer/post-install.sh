ac3a876c49d3816455f3bc7af470753a0f2ac9aa
bartr <bartr@microsoft.com>
2020-09-29 20:04:11 -0500

    codespaces

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -11,14 +11,10 @@ dotnet restore src/tests.sln
 # docker bash-completion
 #sudo curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker
 
-#DEBIAN_FRONTEND=noninteractive
-# update apt-get
-#sudo apt-get update
-#sudo apt-get install -y --no-install-recommends apt-utils dialog
-
-# update / install utils
-#sudo apt-get install -y --no-install-recommends dnsutils httpie bash-completion curl wget git unzip
-#DEBIAN_FRONTEND=dialog
+DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends dnsutils httpie bash-completion curl wget git unzip
+DEBIAN_FRONTEND=dialog
 
 # source the bashrc-append from the repo
 # you can add project specific settings to .bashrc-append and
