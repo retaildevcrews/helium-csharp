e7e67f4856db93c7686540a9396924757de292bb
Akinlolu Akindele <AAkindele@users.noreply.github.com>
2020-10-05 11:50:45 -0500

    Issue #151 update paths in dockerfile to fix codespaces build (#152)
    * issue #151 update paths in dockerfile
    
    * issue #151 add in commented config for making this work when building directly from the docker file. tested in github codespaces and was able to get through helium walkthrough
    
    * testing cleanup changes to dockerfile
    
    * well be sticking with default upstream image for codespaces
    
    * testing that passing in username arg works
    
    * add in parts of upstream script that is handling the username
    
    * adding note about how to get access to docker cli from inside a container
    
    * back to default upstream container. testing that we no longer need to mount socket
    
    * adding more of the original script and updates to the docker comments
    
    * code cleanup from pull request
    
    * updates from pull request. copy directory instead of individual files. only add non-root user to docker group.

--- .devcontainer/library-scripts/docker-debian.sh
+++ .devcontainer/library-scripts/docker-debian.sh
@@ -1,10 +1,36 @@
 #!/usr/bin/env bash
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#-------------------------------------------------------------------------------------------------------------
+#
+# To use the Docker CLI inside a container, use the bind mount option to add the docker socket to the container.
+# Example: "docker run -v '/var/run/docker.sock:/var/run/docker.sock' ..."
+
+USERNAME=${1:-"vscode"}
 
 set -e
 
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
+fi
+
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+    USERNAME=root
 fi
 
 # Install Docker CLI
@@ -17,5 +43,8 @@ apt-get -y install --no-install-recommends docker-ce-cli
 LATEST_COMPOSE_VERSION=$(curl -sSL "https://api.github.com/repos/docker/compose/releases/latest" | grep -o -P '(?<="tag_name": ").+(?=")')
 curl -sSL "https://github.com/docker/compose/releases/download/${LATEST_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+groupadd -g 800 docker
+if [ "${USERNAME}" != "root" ]; then usermod -a -G docker $USERNAME; fi
 
 echo "Done!"
