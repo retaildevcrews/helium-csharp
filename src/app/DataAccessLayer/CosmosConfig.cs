cfa7488169bb9324fc2bf6be3ba37ac24f3e787d
bartr <bartr@microsoft.com>
2020-10-11 12:32:03 -0500

    resolved style cop warnings

--- src/app/DataAccessLayer/CosmosConfig.cs
+++ src/app/DataAccessLayer/CosmosConfig.cs
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using Microsoft.Azure.Cosmos;
 using System;
+using Microsoft.Azure.Cosmos;
 
 namespace CSE.Helium.DataAccessLayer
 {
--- src/app/DataAccessLayer/idal.cs
+++ src/app/DataAccessLayer/idal.cs
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using CSE.Helium.Model;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using CSE.Helium.Model;
 
 namespace CSE.Helium.DataAccessLayer
 {
