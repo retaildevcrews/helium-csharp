f62cd1d8a575b8db76b6c43c4dd2ca19aef0b469
bartr <bartr@microsoft.com>
2021-10-02 06:22:19 +0000

    updated script

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -2,10 +2,6 @@
 
 echo "on-create start" >> ~/status
 
-# copy vscode files
-mkdir -p .vscode
-cp .devcontainer/vscode-template/* .vscode
-
 # run dotnet restore
 dotnet restore src/helium.sln
 dotnet restore src/tests.sln
